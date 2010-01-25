40bb9a3d0ecc486177446c1ceff88e88b7d1ce19
Nate Klaiber <nate@theklaibers.com>
2010-01-25 18:03:10 -0500

    paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,13 @@
-# if RAILS_ENV == "development" 
-#   Paperclip.options[:image_magick_path] = '/opt/local/bin/' 
-# else 
-#   Paperclip.options[:image_magick_path] = '/usr/bin/' 
-# end
+require 'paperclip'
+
+Paperclip.interpolates(:resource_name) do |attachment, style|
+  attachment.instance.channel.resource_name
+end
+
+if RAILS_ENV == "development" 
+  Paperclip.options[:image_magick_path] = '/opt/local/bin/' 
+else 
+  Paperclip.options[:image_magick_path] = '/usr/local/bin' 
+end
+
+Paperclip.options[:log_command] = true
