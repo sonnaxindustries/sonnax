ded336dedeea7cb09794ad6cfd5aaf6fff0b5cc4
Nate Klaiber <nate@theklaibers.com>
2010-05-03 07:44:02 -0400

    fixing paths in the helper files

--- spec/views/product_lines/show.html.erb_spec.rb
+++ spec/views/product_lines/show.html.erb_spec.rb
@@ -1,12 +1,5 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/product_lines/show" do
-  before(:each) do
-    render 'product_lines/show'
-  end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/product_lines/show])
-  end
 end
