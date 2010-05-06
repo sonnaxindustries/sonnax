06db7ba6c203b12a0ba79b2b93c76cf4ba00d67c
Nate Klaiber <nate@theklaibers.com>
2010-05-06 15:16:30 -0400

    updating the factory

--- spec/factories/distributor_factory.rb
+++ spec/factories/distributor_factory.rb
@@ -1,8 +1,9 @@
 Factory.define(:distributor) do |f|
   f.name 'Transmission Parts, Inc'
   f.url_friendly 'transmission-parts-inc'
-  f.postal_code_id 1
-  f.country_id 3
+  f.city 'city name'
+  f.state 'state name'
+  f.country 'country name'
   f.phone_number '33-222-3333'
   f.website_url 'http://example.com'
 end
