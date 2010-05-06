6780791153e626f077dfbe11588256d7870513c9
Nate Klaiber <nate@theklaibers.com>
2010-05-05 23:39:24 -0400

    adding the user and role converter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.merge_validates_length_of_login_field_options(:within => 2..50)
+  end
+  
+  has_many :users_roles
+  has_many :roles, :through => :users_roles
 end
--- app/models/users_role.rb
+++ app/models/users_role.rb
@@ -1,2 +1,4 @@
 class UsersRole < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :role
 end
