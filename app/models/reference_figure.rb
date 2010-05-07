afe3a92a2165bda56a23e06a98268f7afca8bb66
Nate Klaiber <nate@theklaibers.com>
2010-05-07 14:58:42 -0400

    adding the default URL for a missing image

--- app/models/reference_figure.rb
+++ app/models/reference_figure.rb
@@ -1,7 +1,7 @@
 class ReferenceFigure < ActiveRecord::Base
   has_attached_file :avatar,
                     :styles => { :thumbnail => '100x100', :medium => '300x300', :tiny => '30x30#' },
-                    :default_url => '/system/avatars/tiny/missing.gif'
+                    :default_url => '/system/avatars/:style/missing.gif'
   has_attached_file :exploded_view
   
   named_scope :list
