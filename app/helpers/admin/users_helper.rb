8b771479a71d159b4313ad014633d2f451bebcd1
Nate Klaiber <nate@theklaibers.com>
2010-05-07 13:54:36 -0400

    show the users helpers

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,17 @@
 module Admin::UsersHelper
+  def last_login_at(user)
+    if user.last_login_at?
+      user.last_login_at.to_s(:month_day_year)
+    else
+      '<span class="inactive">Never logged in</span>'
+    end
+  end
+  
+  def login_count(user)
+    if user.login_count?
+      pluralize(user.login_count, 'times')
+    else
+      '<span class="inactive">Never logged in</span>'
+    end
+  end
 end
