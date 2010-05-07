01dde8bff6cff660d08b7f9acf0f5e467923425e
Nate Klaiber <nate@theklaibers.com>
2010-05-07 14:56:52 -0400

    missing image for the small photo

--- app/helpers/admin/reference_figures_helper.rb
+++ app/helpers/admin/reference_figures_helper.rb
@@ -1,7 +1,3 @@
 module Admin::ReferenceFiguresHelper
-  def photo_for(reference_figure)
-    if reference_figure.avatar?
-      image_tag reference_figure.avatar.url(:tiny) , :class => 'tiny-reference-figure'     
-    end
-  end
+
 end
--- app/models/reference_figure.rb
+++ app/models/reference_figure.rb
@@ -1,6 +1,7 @@
 class ReferenceFigure < ActiveRecord::Base
   has_attached_file :avatar,
-                    :styles => { :thumbnail => '100x100', :medium => '300x300', :tiny => '30x30#' }
+                    :styles => { :thumbnail => '100x100', :medium => '300x300', :tiny => '30x30#' },
+                    :default_url => '/system/avatars/tiny/missing.gif'
   has_attached_file :exploded_view
   
   named_scope :list
