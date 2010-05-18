63541fac04b38cc76f14811aef85124820d5205a
Nate Klaiber <nate@theklaibers.com>
2010-05-17 23:27:54 -0400

    adding gems for spreadsheet import

--- lib/import/excel/publication.rb
+++ lib/import/excel/publication.rb
@@ -1,16 +1,96 @@
-require 'rubygems'
-require 'roo'
-require 'ostruct'
-
 class Import::Excel::Publication
-  attr_accessor :file_name, :start_row, :start_column
+    
+  class Row
+    def initialize(attrs={})
+      @attributes = attrs
+    end
+    
+    def title
+      @title ||= @attributes[:title]
+    end
+    
+    def category
+      @category ||= @attributes[:publication_category]
+    end
+    
+    def type
+      @type ||= @attributes[:publication_type]
+    end
+    
+    def subject
+      @subject ||= @attributes[:subject]
+    end
+    
+    def date
+      return nil if @attributes[:date].blank?
+      @date ||= Date.parse(@attributes[:date])
+    end
+    
+    def author
+      @author ||= @attributes[:author]
+    end
+    
+    def pdf_filename
+      @pdf_filename ||= @attributes[:pdf_filename]
+    end
+    
+    def pdf_filename?
+      !self.pdf_filename.blank?
+    end
+    
+    def pdf
+      File.new(File.join(Rails.root, 'public', 'file_conversions', 'tech-articles', self.pdf_filename))
+    end
+    
+    def pdf?
+      !self.pdf.blank?
+    end
+    
+    def product_line_id
+      @product_line_id ||= @attributes[:product_line_id]
+    end
+    
+    def product_line
+      Legacy::ProductLine.find(self.product_line_id)
+    end
+    
+    def make_name
+      @make_name ||= @attributes[:make_name]
+    end
+    
+    def make
+      Legacy::Make.find_by_make(self.make_name)
+    end
+    
+    def unit_name
+      @unit_name ||= @attributes[:unit_name]
+    end
+    
+    def unit
+      Legacy::Unit.find_by_name(self.unit_name)
+    end
+    
+    def keywords
+      @keywords ||= @attributes[:keywords]
+    end
+  end
   
   def initialize
-    self.file_name = '/users/nateklaiber/downloads/publication_data.xls'
+    @file_name = File.join(Rails.root, 'lib', 'import', 'excel', 'docs', 'publication_data.xls')
+    self.setup_sheet!
+  end
+  
+  def setup_sheet!
+    self.workbook.default_sheet = self.publications_worksheet
+  end
+  
+  def import!
+    self.records.each do |publication|
+    end
   end
   
   def workbook
-    @workbook ||= Excel.new(self.file_name)
+    @workbook ||= Excel.new(@file_name)
   end
   
   def start_column
@@ -24,36 +104,52 @@ class Import::Excel::Publication
   def publications_worksheet
     @publications_worksheet ||= self.workbook.sheets[2]
   end
+  
+  def publication_categories
+    @publication_categories ||= self.records.map(&:category).uniq.compact
+  end
+  
+  def publication_types
+    @publication_types ||= self.records.map(&:type).uniq.compact
+  end
+  
+  def publication_subjects
+    @publication_subjects ||= self.records.map(&:subject).uniq.compact
+  end
+  
+  def publication_authors
+    @publication_authors ||= self.records.map(&:author).uniq.compact
+  end
+  
+  def records
+    self.parse_records!
+    @record_objects
+  end
+  
+  def parse_records!
+    @record_objects = []
+    (self.start_row..self.workbook.last_row).each_with_index do |index,row|
+      @record_objects << Row.new(
+                               :title_id              => self.workbook.cell(index, 2),
+                               :publication_category  => self.workbook.cell(index, 3),
+                               :publication_type      => self.workbook.cell(index, 4),
+                               :title                 => self.workbook.cell(index, 5),
+                               :subject               => self.workbook.cell(index, 6),
+                               :date                  => self.workbook.cell(index, 7),
+                               :author                => self.workbook.cell(index, 8),
+                               :pdf_filename          => self.workbook.cell(index, 9),
+                               :product_line_id       => self.workbook.cell(index, 10),
+                               :make_name             => self.workbook.cell(index, 11),
+                               :unit_name             => self.workbook.cell(index, 12),
+                               :keywords => OpenStruct.new(:rebuilding  => self.workbook.cell(index, 13),
+                                                           :diagnosis   => self.workbook.cell(index, 14),
+                                                           :complaint   => self.workbook.cell(index, 15),
+                                                           :correction  => self.workbook.cell(index, 16)
+                              )
+                )
+      @record_objects
+    end
+  end
 end
 
 
-# 
-# file_name = 
-# workbook = Excel.new(file_name)
-# start_row = 3
-# start_column = 2
-# 
-# workbook.default_sheet = workbook.sheets[2]
-# 
-# # iterate over the rows
-# # import all of the uniques
-# records = []
-# (start_row..workbook.last_row).each_with_index do |index,row|
-#   records << OpenStruct.new(:title_id => workbook.cell(index, 2),
-#                            :publication_category => workbook.cell(index, 3),
-#                            :publication_type => workbook.cell(index, 4),
-#                            :title => workbook.cell(index, 5),
-#                            :subject => workbook.cell(index, 6),
-#                            :date => workbook.cell(index, 7),
-#                            :author => workbook.cell(index, 8),
-#                            :pdf => workbook.cell(index, 9),
-#                            :product_line_id => workbook.cell(index, 10),
-#                            :make => workbook.cell(index, 11),
-#                            :unit => workbook.cell(index, 12),
-#                            :keywords => OpenStruct.new(:rebuilding => workbook.cell(index, 13),
-#                                                        :diagnosis => workbook.cell(index, 14),
-#                                                        :complaint => workbook.cell(index, 15),
-#                                                        :correction => workbook.cell(index, 16)
-#                           )
-#             )
-# end
