1afbce028a104483a658d1e82ff7cd00398cdd47
Nate Klaiber <nate@theklaibers.com>
2010-06-23 17:18:52 -0400

    Adding some helpers and IE6 stylesheet

--- app/helpers/admin/publication_titles_helper.rb
+++ app/helpers/admin/publication_titles_helper.rb
@@ -6,4 +6,20 @@ module Admin::PublicationTitlesHelper
       '<span class="inactive">Not Provided</span>'
     end
   end
+  
+  def publication_published_at(publication)
+    if publication.published_at?
+      publication.published_at.to_s(:month_year)
+    else
+      '<span class="inactive">Not Provided</span>'
+    end
+  end
+  
+  def publication_authors_list(publication)
+    if publication.authors?
+      publication.authors.map { |author| author.full_name }.to_sentence
+    else
+      '<span class="inactive">Not Provided</span>'
+    end
+  end
 end
