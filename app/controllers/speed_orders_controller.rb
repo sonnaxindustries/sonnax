25727da7781ca68000e547b9a936d451c13848c6
Nate Klaiber <nate@theklaibers.com>
2010-11-10 17:45:29 -0500

    Ensuring the dropdown gets preselected based on the previous product line page

--- app/controllers/speed_orders_controller.rb
+++ app/controllers/speed_orders_controller.rb
@@ -1,9 +1,10 @@
 class SpeedOrdersController < ApplicationController
+  
   def new
     @cart = find_cart
     @speed_order = SpeedOrder.new
     @product_line_options = ProductLine.speed_order_options
-    @selected_product_line = ProductLine.torque_converter
+    @selected_product_line = retrieve_selected_product_line 
   end
   
   def create
@@ -19,6 +20,14 @@ class SpeedOrdersController < ApplicationController
       @selected_product_line = ProductLine.find(params[:speed_order][:product_line_id])
       flash[:errors] = @cart.errors
       redirect_to(cart_path)
+    end
+  end
+private
+  def retrieve_selected_product_line
+    begin
+      ProductLine.find_by_id!(params[:from])
+    rescue ActiveRecord::RecordNotFound
+      ProductLine.torque_converter
     end
   end
 end
