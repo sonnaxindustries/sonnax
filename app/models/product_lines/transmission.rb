5e6c185c54ec438bc42a9b912fbdc6d009414652
Nate Klaiber <nate@theklaibers.com>
2010-11-15 18:49:00 -0500

    Fixing a bug in the transmission part search, to only search parts scoped by product line

--- app/models/product_lines/transmission.rb
+++ app/models/product_lines/transmission.rb
@@ -22,7 +22,7 @@ class ProductLines::Transmission < ProductLine
   end
   
   def search_parts(search_term)
-    Part.search_by_filter(search_term, :product_line => @self)
+    Part.search_by_filter(search_term, :product_line => self)
   end
 
   def parts_by_filter(options={})
