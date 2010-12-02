af8f6b23342450827909ae4472a0ad928600f923
Nate Klaiber <nate@theklaibers.com>
2010-12-01 23:28:03 -0500

    Paperclip Config settings

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,10 +4,10 @@ Paperclip.interpolates(:resource_name) do |attachment, style|
   attachment.instance.channel.resource_name
 end
 
-if RAILS_ENV == "development" 
+if Rails.env.development?
   Paperclip.options[:command_path] = '/opt/local/bin/' 
 else 
-  Paperclip.options[:command_path] = '/usr/local/bin' 
+  Paperclip.options[:command_path] = '/usr/local/bin/' 
 end
 
 Paperclip.options[:log_command] = true
