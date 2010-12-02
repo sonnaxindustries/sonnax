c6a16d2777a7f9b2e8d1de80893a118fec6102d1
Nate Klaiber <nate@theklaibers.com>
2010-12-01 23:50:54 -0500

    paperclip fixes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,10 +5,9 @@ Paperclip.interpolates(:resource_name) do |attachment, style|
 end
 
 if Rails.env.development?
-  Paperclip.options[:command_path] = '/opt/local/bin' 
+  Paperclip.options.merge!(:command_path => '/opt/local/bin')
 else 
-  Paperclip.options[:command_path] = '/usr/local/bin' 
+  Paperclip.options.merge!(:command_path => '/usr/local/bin')
 end
 
-Paperclip.options[:log_command] = true
-Paperclip.options[:swallow_stderr] = false
+Paperclip.options.merge!(:log_command => true)
