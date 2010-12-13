7c4e7a2d1b3fe2d8041161cbabdcb6cb69d428b5
Nate Klaiber <nate@theklaibers.com>
2010-12-12 20:44:14 -0500

    Adding the asset_src for extra documents with parts

--- app/models/admin/part_asset.rb
+++ app/models/admin/part_asset.rb
@@ -1,9 +1,11 @@
 class Admin::PartAsset < PartAsset
+  attr_accessor :asset_src
+
   belongs_to :part, :class_name => 'Admin::Part'
   belongs_to :part_asset_type, :class_name => 'Admin::PartAssetType'
   belongs_to :asset, :class_name => 'Admin::Asset'
   
-  #accepts_nested_attributes_for :asset
+  accepts_nested_attributes_for :asset
   
   validates_uniqueness_of :part_id, :scope => [:part_asset_type_id, :asset_id]
   
@@ -18,8 +20,13 @@ class Admin::PartAsset < PartAsset
   end
 
   def asset_src=(val)
-    asset_obj = Admin::Asset.create(:asset => val)
-    self.update_attributes(:asset => asset_obj)
-    #self.create_asset(:asset => val)
+    filename = val['asset']
+    if self.new_record?
+      asset_obj = Admin::Asset.create(:asset => filename)
+      self.asset = asset_obj
+    else
+      self.asset.update_attributes(:asset => filename)
+    end
   end
+
 end
