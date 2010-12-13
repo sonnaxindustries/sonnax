60120a49d26a608210ca87e88b1b1f1a425dfc43
Nate Klaiber <nate@theklaibers.com>
2010-12-12 19:42:43 -0500

    ordering the author publications

--- app/models/publication_author.rb
+++ app/models/publication_author.rb
@@ -1,17 +1,18 @@
 class PublicationAuthor < ActiveRecord::Base
-  has_many :publication_authors, :class_name => 'PublicationTitlesAuthor', :dependent => :destroy
-  has_many :publications, :through => :publication_authors
-  
+  has_many :publication_titles, :class_name => 'PublicationTitlesAuthor', :dependent => :destroy
+  has_many :publications, :order => 'published_at DESC', :through => :publication_titles
+  has_many :ordered_publications, :order => 'published_at DESC', :source => :publication, :through => :publication_titles
+
   class << self
     def detail!(id)
-      self.find_by_id!(id, :include => [:publications])
+      self.find_by_id!(id, :include => [:ordered_publications])
     end
 
     def options
       self.list
     end
   end
-  
+
   def publications?
     self.publications.any?
   end
