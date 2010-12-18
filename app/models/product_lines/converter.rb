28f99c08ecabbdc6950460d61136192696e7f4cf
Nate Klaiber <nate@theklaibers.com>
2010-12-18 14:09:00 -0500

    Adding a new search parts for torque converter

--- app/models/product_lines/converter.rb
+++ app/models/product_lines/converter.rb
@@ -16,15 +16,28 @@ class ProductLines::Converter < ProductLine
     attrs.reverse_merge!(:product_line => self)
     self.collection_presenter.constantize.new(attrs)
   end
-  
+
   def recent_parts
     self.parts.recent.ordered('parts.part_number ASC')
   end
-  
-  def search_parts(search_term)
+
+  def search_parts(keyword)
+    Part.find(:all,
+              :select => 'DISTINCT(m.id) AS make_id, m.name AS make_name, p.*',
+              :from => 'parts p',
+              :joins => "LEFT JOIN product_lines pl ON pl.id = p.product_line_id
+                         LEFT JOIN unit_components uc ON uc.part_id = p.id
+                         LEFT JOIN units u ON u.id = uc.unit_id
+                         LEFT JOIN units_makes um ON um.unit_id = uc.unit_id
+                         LEFT JOIN makes m ON m.id = um.make_id",
+              :conditions => ["p.part_number LIKE ? AND u.name IS NOT NULL AND m.id IS NOT NULL AND p.product_line_id = ?", "#{keyword}%", self],
+              :order => 'item ASC, m.name ASC')
+  end
+
+  def search_parts_old(search_term)
     Part.search_by_filter(search_term, :product_line => self, :order => 'p.part_number ASC')
   end
-  
+
   def parts_by_filter(options={})
     options.reverse_merge!(:product_line => self) 
     @parts ||= Part.find_by_filter(options || {})
