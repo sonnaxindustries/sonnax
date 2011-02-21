f2e4fc84ed32cd2a4783d7342a491493e9b2f4dc
Nate Klaiber <nate@theklaibers.com>
2011-02-21 10:14:24 -0500

    adding lilas updated application CSS

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,8 +5,8 @@ Paperclip.interpolates(:resource_name) do |attachment, style|
 end
 
 if Rails.env.development?
-  #Paperclip.options.merge!(:command_path => '/opt/local/bin')
-  Paperclip.options.merge!(:command_path => '/usr/local/bin')
+  Paperclip.options.merge!(:command_path => '/opt/local/bin')
+  #Paperclip.options.merge!(:command_path => '/usr/local/bin')
 else 
   Paperclip.options.merge!(:command_path => '/usr/local/bin')
 end
